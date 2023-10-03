5db9853a46e1f3192097e8857d1d0035e99c63a5
nathaliaveneziano <nathaliaveneziano@icloud.com>
2023-10-03 18:32:11 -0300

    implementing single.php

--- template-parts/post/content.php
+++ template-parts/post/content.php
@@ -7,7 +7,13 @@
 
 <article id="post-<?php the_ID(); ?>">
   <header class="entry-header">
-    <?php the_title('<h2 class="entry-title"><a class="entry-link" href="' . esc_url(get_permalink()) . '">', '</a></h2>'); ?>
+    <?php
+    if (is_singular()) :
+      the_title('<h1 class="entry-title">', '</h1>');
+    else :
+      the_title('<h2 class="entry-title"><a class="entry-link" href="' . esc_url(get_permalink()) . '">', '</a></h2>');
+    endif;
+    ?>
   </header>
 
   <!-- Post thumbnail -->
@@ -19,6 +25,17 @@
 
   <!-- Post content -->
   <div class="entry-content">
-    <?php the_excerpt(); ?>
+    <?php
+    if (is_home() || is_archive()) :
+      the_excerpt();
+    elseif (is_single()) :
+      the_content();
+
+      wp_link_pages(array(
+        'before' => '<div class="page-links">' . esc_html__('Pages:', 'herobiz'),
+        'after' => '</div>',
+      ));
+    endif;
+    ?>
   </div>
 </article>
